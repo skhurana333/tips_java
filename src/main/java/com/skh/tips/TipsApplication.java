25296a6ad72b19b15e4ebd4ff55d53fa46e9e315
skhurana1 <sandeep_khurana@intuit.com>
2022-03-12 22:35:56 +0530

    changed for java

--- src/main/java/com/skh/tips/TipsApplication.java
+++ src/main/java/com/skh/tips/TipsApplication.java
@@ -1,13 +1,36 @@
 package com.skh.tips;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TipsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TipsApplication.class, args);
+	}
+
+	//HTTP port
+	@Value("${http.port}")
+	private int httpPort;
+
+	// Let's configure additional connector to enable support for both HTTP and HTTPS
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
 	}
 
 }
